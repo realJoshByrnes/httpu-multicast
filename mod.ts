a3b6a1316483b9b7619d5b1ff8e455a16a2f031f
Josh Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2024-12-23 20:02:31 +1100

    Bump version to 0.1.1 and add documentation for HttpuMulticast class

--- mod.ts
+++ mod.ts
@@ -1,14 +1,38 @@
-import HttpuCore from "@realjoshbyrnes/httpu-core";
+/**
+ * @module HttpuMulticast
+ * @description This module provides the HttpuMulticast class which extends the HttpuCore class.
+ */
 
+import HttpuCore from "./HttpuCore";
+
+/**
+ * Class representing an HTTPU Multicast.
+ * @extends HttpuCore
+ */
 export default class HttpuMulticast extends HttpuCore {
+  /**
+   * Create an HttpuMulticast instance.
+   * @param {string | URL} url - The URL to send the request to.
+   * @param {string} [method=NOTIFY] - The HTTP method to use.
+   */
   constructor(url: string | URL, method: string = "NOTIFY") {
     super(url, method);
   }
 
+  /**
+   * Get the default path for the request.
+   * @protected
+   * @returns {string} The default path.
+   */
   protected get _defaultPath(): string {
     return '*';
   }
 
+  /**
+   * Get the default protocol for the request.
+   * @protected
+   * @returns {string} The default protocol.
+   */
   protected get _defaultProtocol(): string {
     return 'httpmu:';
   }
